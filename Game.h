dbd8155e349681bb85e4c61ded195e4f899d6113
dotrungducUET <160621205+dotrungducUET@users.noreply.github.com>
2024-03-02 21:06:26 +0800

    Add previously coded headers that create empty window

---
+++ Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H_INCLUDED
+#define GAME_H_INCLUDED
+
+#include "GameConstants.h"
+#include <SDL.h>
+
+class Game
+{
+public:
+    Game();
+    bool Init();
+    void Run();
+private:
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+    SDL_Event event;
+};
+
+#endif // GAME_H_INCLUDED
